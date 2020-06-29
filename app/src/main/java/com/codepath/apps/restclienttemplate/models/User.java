63bca26f2315bc89207aae34ab8ba300ec3782c8
Omar Guajardo <omarguajardo@fb.com>
2020-06-29 14:13:47 -0700

    Tweet and User model added as well as a basic TimeLine Activity with a RV and made a simple item_tweet.xml

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,30 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+
+    public String name;
+    public String handle;
+    public String profileImageUrl;
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+        user.name = jsonObject.getString("name");
+        user.handle = jsonObject.getString("screen_name");
+        user.profileImageUrl = jsonObject.getString("profile_image_url_https");
+        return user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+}
