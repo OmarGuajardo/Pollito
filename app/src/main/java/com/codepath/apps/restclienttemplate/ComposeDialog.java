861425a2eecfb1f09c780425f104b9122fc80885
Omar Guajardo <omarguajardo@fb.com>
2020-06-30 12:40:24 -0700

    File cleanup

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeDialog.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeDialog.java
@@ -3,21 +3,16 @@ package com.codepath.apps.restclienttemplate;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
-
-import com.codepath.apps.restclienttemplate.R;
 
 public class ComposeDialog extends AppCompatDialogFragment {
 
@@ -29,7 +24,7 @@ public class ComposeDialog extends AppCompatDialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view  = inflater.inflate(R.layout.layout_dialog,null);
+        View view  = inflater.inflate(R.layout.compose_layout,null);
 
         builder.setView(view);
 
