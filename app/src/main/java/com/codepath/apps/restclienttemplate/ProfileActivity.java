74ce9663592e20ebfb1d0de8150c01e314639b6d
Omar Guajardo <omarguajardo@fb.com>
2020-07-02 18:31:23 -0700

    basic motion done

--- app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
@@ -3,14 +3,15 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
 import com.codepath.apps.restclienttemplate.databinding.ActivityProfileBinding;
 import com.google.android.material.tabs.TabLayout;
 
+//public class ProfileActivity extends AppCompatActivity  {
 public class ProfileActivity extends AppCompatActivity  {
-//public class ProfileActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
 ActivityProfileBinding binding;
 Toolbar toolbar;
 ViewPager viewPager;
@@ -22,32 +23,34 @@ TabLayout tabLayout;
         setContentView(binding.getRoot());
         setSupportActionBar(binding.mainToolbar);
 
-//        tabLayout = binding.tabLayout;
-//        viewPager = binding.viewPager;
-//
-//        tabLayout.addTab(tabLayout.newTab().setText("Following"));
-//        tabLayout.addTab(tabLayout.newTab().setText("Followers"));
-//
-//
-//        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
-//
-//        viewPager.setAdapter(pagerAdapter);
-//
-//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-//    }
-//
-//    @Override
-//    public void onTabSelected(TabLayout.Tab tab) {
-//        viewPager.setCurrentItem(tab.getPosition());
-//    }
-//
-//    @Override
-//    public void onTabUnselected(TabLayout.Tab tab) {
-//
-//    }
-//
-//    @Override
-//    public void onTabReselected(TabLayout.Tab tab) {
-//
-//    }
-}}
+        tabLayout = binding.tabLayout;
+        viewPager = binding.viewPager;
+
+
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+}
